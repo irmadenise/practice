e6b5ada6ca36b67571612d00cecadcfdb35c9628
Irma Settle <irma.settle@gmail.com>
2021-09-30 15:24:24 -0700

    practice commit

---
+++ src/Practice.java
@@ -0,0 +1,20 @@
+
+public class Practice {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+
+	int i = 20;
+	if (i == 10) {
+		System.out.println("i is 10");
+	} else if (i == 15) {
+	System.out.println("i is 15");
+	}	else if (i == 20) {
+	System.out.println("i is 20");
+
+	} else {
+	System.out.println("i is not a valid option");
+	}
+	}
+}
